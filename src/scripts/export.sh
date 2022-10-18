de064c04cd40a7ebc7ed50b1552b3016cbe145a7
Eddy Filip <eddy.filip@agilebits.com>
2022-10-18 13:39:27 +0200

    Try to fix shell error

--- src/scripts/export.sh
+++ src/scripts/export.sh
@@ -1,7 +1,7 @@
 # shellcheck disable=SC2148
 random_heredoc_identifier=$(env LC_ALL=C tr -dc 'a-zA-Z0-9' < /dev/urandom | fold -w 64 | head -n 1) || true
 {
-    printf "export %s=$(cat '\<<'" ""${PARAM_VAR_NAME}""
+    printf "export %s=$(cat '<<'" ""${PARAM_VAR_NAME}""
     printf "%s'\n'" ""${random_heredoc_identifier}""
     op read ""${PARAM_SECRET_REFERENCE}""
     printf "%s'\n')\n" "${random_heredoc_identifier}"
