f350afd9d3570fdbaaa02d5cf2e1dcaea8562f8e
Eddy Filip <eddy.filip@agilebits.com>
2022-10-18 14:37:18 +0200

    Fix exec script

--- src/scripts/exec.sh
+++ src/scripts/exec.sh
@@ -1,5 +1,5 @@
 # shellcheck disable=SC2148
-if [ -z "${PARAM_FLAGS}" ]; then
+if [ -n "${PARAM_FLAGS}" ]; then
     op run "${PARAM_FLAGS}" -- "$SHELL" -c "${PARAM_COMMAND}"
 else
     op run -- "$SHELL" -c "${PARAM_COMMAND}"
