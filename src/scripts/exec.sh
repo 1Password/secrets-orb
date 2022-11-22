c16ec12185272afbba5fc22b7f4ae3af930019f7
Eddy Filip <eddy.filip@agilebits.com>
2022-11-22 18:11:17 +0100

    Pass User-Agent information to the CLI

--- src/scripts/exec.sh
+++ src/scripts/exec.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+
+# User-Agent info for 1Password CLI
+OP_INTEGRATION_NAME="1Password CircleCI Secrets Orb"
+OP_INTEGRATION_ID="CIR"
+OP_INTEGRATION_BUILDNUMBER="1000001"
+
 if [ -n "${PARAM_FLAGS}" ]; then
     op run "${PARAM_FLAGS}" -- "$SHELL" -c "${PARAM_COMMAND}"
 else
