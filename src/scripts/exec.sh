a3fc2767f9c79c388a2dc7b05ec5f92e0993eefc
Eddy Filip <eddy.filip@agilebits.com>
2022-11-22 18:12:40 +0100

    Fix exec script

--- src/scripts/exec.sh
+++ src/scripts/exec.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # User-Agent info for 1Password CLI
-OP_INTEGRATION_NAME="1Password CircleCI Secrets Orb"
-OP_INTEGRATION_ID="CIR"
-OP_INTEGRATION_BUILDNUMBER="1000001"
+export OP_INTEGRATION_NAME="1Password CircleCI Secrets Orb"
+export OP_INTEGRATION_ID="CIR"
+export OP_INTEGRATION_BUILDNUMBER="1000001"
 
 if [ -n "${PARAM_FLAGS}" ]; then
     op run "${PARAM_FLAGS}" -- "$SHELL" -c "${PARAM_COMMAND}"
